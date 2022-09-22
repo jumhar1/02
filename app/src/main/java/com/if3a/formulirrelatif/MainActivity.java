b431268f87cd6155ad0666ad79068d401e77c472
brodytiktok <naufalakbar@mhs.mdp.ac.id>
2022-09-22 16:10:33 +0700

    Initial commit

---
+++ app/src/main/java/com/if3a/formulirrelatif/MainActivity.java
@@ -0,0 +1,67 @@
+package com.if3a.formulirrelatif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etNpm, etNama;
+    private RadioGroup rgJk;
+    private RadioButton rbJk;
+    private Spinner spProdi;
+    private Button btnDaftar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNpm = findViewById(R.id.et_npm);
+        etNama = findViewById(R.id.et_nama);
+        rgJk = findViewById(R.id.rg_jk);
+        spProdi = findViewById(R.id.sp_prodi);
+        btnDaftar = findViewById(R.id.btn_daftar);
+
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String npm, nama, jk,  prodi;
+                int selectedID;
+
+                npm = etNpm.getText().toString();
+                nama = etNama.getText().toString();
+                prodi = spProdi.getSelectedItem().toString();
+
+                selectedID = rgJk.getCheckedRadioButtonId();
+                rbJk = findViewById(selectedID);
+                jk = rbJk.getText().toString();
+
+                if (npm.trim().equals("")){
+                    etNpm.setError("NPM tidak boleh Kosong");
+                }
+                else if (nama.trim().equals("")){
+                    etNama.setError("Nama Tidak Boleh Kosong");
+                }
+                else {
+                    Intent intent = new
+                            Intent(MainActivity.this, ResultActivity.class);
+                    intent.putExtra("varNPM", npm);
+                    intent.putExtra("varNama", nama);
+                    intent.putExtra("varJK", jk);
+                    intent.putExtra("varProdi", prodi);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
+}
